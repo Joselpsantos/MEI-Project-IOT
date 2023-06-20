8af028bbc032cce4e149b30ac673b408abee98f5
Joselpsantos <joselpsantoss@gmail.com>
2023-06-20 22:10:09 +0100

    Started refactoring the html templates and the backend

--- WebPage/app.py
+++ WebPage/app.py
@@ -1,82 +1,88 @@
-from flask import Flask, request, jsonify, render_template, redirect
+import eventlet
+import json
+from flask import Flask, render_template
 from flask_mqtt import Mqtt
-from flask_sockets import Sockets
+from flask_socketio import SocketIO
+from flask_bootstrap import Bootstrap
+
+eventlet.monkey_patch()
 
 app = Flask(__name__)
-sockets = Sockets(app)
-
-app.config['MQTT_BROKER_URL'] = '192.168.45.10'
+#app.config['SECRET'] = 'my secret key'
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config['MQTT_BROKER_URL'] = '192.168.1.123'
 app.config['MQTT_BROKER_PORT'] = 1883
-app.config['MQTT_USERNAME'] = 'pico'  # Set this item when you need to verify username and password
-app.config['MQTT_PASSWORD'] = 'pico'  # Set this item when you need to verify username and password
-app.config['MQTT_KEEPALIVE'] = 5  # Set KeepAlive time in seconds
-app.config['MQTT_TLS_ENABLED'] = False  # If your server supports TLS, set it True
-topic = 'jose'
-received_message = ""
-led_status = ""
-mqtt_client = Mqtt(app)
+app.config['MQTT_USERNAME'] = 'pico'
+app.config['MQTT_PASSWORD'] = 'pico'
+app.config['MQTT_KEEPALIVE'] = 5
+app.config['MQTT_TLS_ENABLED'] = False
 
-@mqtt_client.on_connect()
+topic1 = "temperature"
+topic2 = "control"
+
+mqtt = Mqtt(app)
+socketio = SocketIO(app)
+bootstrap = Bootstrap(app)
+
+@mqtt.on_connect()
 def handle_connect(client, userdata, flags, rc):
    if rc == 0:
        print('Connected successfully')
-       mqtt_client.subscribe(topic) # subscribe topic
-       global received_message
-       global led_status
-       received_message = ""
-       led_status = ""
+       mqtt.subscribe(topic1)
+       mqtt.subscribe(topic2) # subscribe topic
+        # subscribe topic
    else:
        print('Bad connection. Code:', rc)
        
-@mqtt_client.on_message()
+       
+@app.route('/')
+def index():
+    return render_template('index.html')
+    
+@socketio.on("ledControl")
+def handle_connect(message):
+    mqtt.publish(topic1, message)
+    print(message)
+    
+@socketio.on('connect')
+def handle_connect():
+    
+    print("Connected to the frontend")
+
+@socketio.on('publish')
+def handle_publish(json_str):
+    data = json.loads(json_str)
+    mqtt.publish(topic1, data['message'])
+
+@app.route('/config')
+def config():
+    render_template("config_mqtt.html")
+
+
+#@socketio.on('subscribe')
+#def handle_subscribe():
+#    mqtt.subscribe(topic) 
+#    print("Subscrito")
+
+
+@socketio.on('unsubscribe_all')
+def handle_unsubscribe_all():
+    mqtt.unsubscribe_all()
+
+
+@mqtt.on_message()
 def handle_mqtt_message(client, userdata, message):
+    data = dict(
+        topic=message.topic,
+        payload=message.payload.decode()
+    )
+    socketio.emit('receive', data=data)
+    print(message)
 
-   global received_message
-   global led_status
-   msg = message.payload.decode()
-   if msg == 'on' or msg == 'off':
-      led_status = msg
-      #sockets.send(led_status)
-      
+@mqtt.on_log()
+def handle_logging(client, userdata, level, buf):
+    print(level, buf)
 
-   else:
-      received_message = msg
-      #sockets.send(received_message)
-      
-@app.route('/publish', methods=['POST'])
-def publish_message():
-   request_data = request.get_json()
-   publish_result = mqtt_client.publish(request_data['topic'], request_data['msg'])
-   return jsonify({'code': publish_result[0]})
 
 if __name__ == '__main__':
-   app.run(host='127.0.0.1', port=5000, debug=True)      
-   
-@sockets.route('/socket')
-def ws_conn(ws):
-   while not ws.closed:
-      #message = ws.receive()
-      ws.send(received_message)  # This is required to keep the WebSocket connection open
-
-@app.route('/')
-def home():
-   global received_message
-   global led_status
-   return render_template('index.html', received_message = received_message, led_status = led_status)
-
-@app.route('/something')
-def something():
-   global received_message
-   global led_status
-   return render_template('something.html')
-
-@app.route('/LED/ON')
-def on():
-   mqtt_client.publish(topic, 'on')
-   return redirect('/')
-   
-@app.route('/LED/OFF')
-def off():
-   mqtt_client.publish(topic, 'off')
-   return redirect('/')
-   
+    socketio.run(app, host='127.0.0.1', port=5001, use_reloader=False, debug=True)
