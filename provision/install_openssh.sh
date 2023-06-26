5f930787510b58e2a420a8a4cd5ea4e4251660e2
Joselpsantos <joselpsantoss@gmail.com>
2023-06-26 15:31:27 +0100

    Minor changes in install openssh file

--- provision/install_openssh.sh
+++ provision/install_openssh.sh
@@ -1,8 +1,8 @@
 sudo apt install openssh-server
 
-mkdir duckdns
+mkdir /home/duckdns
 
-cd duckdns
+cd /home/duckdns
 
 echo "echo url="https://www.duckdns.org/update?domains=exampledomain&token=a7c4d0ad-114e-40ef-ba1d-d217904a50f2&ip=" | curl -k -o ~/duckdns/duck.log -K -" > duck.sh
 
