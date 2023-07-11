678f139d599103889347cbb94ee338c7481f8044
Joselpsantos <joselpsantoss@gmail.com>
2023-07-11 13:59:24 +0100

    Checkpoint

--- WebPage/app.py
+++ WebPage/app.py
@@ -1,91 +1,71 @@
-import eventlet
-import json
-import netifaces
-#import netifaces
 from flask import Flask, render_template
-from flask_mqtt import Mqtt
+import paho.mqtt.client as mqtt
 from flask_socketio import SocketIO
 from flask_bootstrap import Bootstrap
+import netifaces
 
-#eventlet.monkey_patch()
 
 app = Flask(__name__)
-#app.config['SECRET'] = 'my secret key'
-app.config['TEMPLATES_AUTO_RELOAD'] = True
-app.config['MQTT_BROKER_URL'] = '127.0.0.1'
-app.config['MQTT_BROKER_PORT'] = 1883
-app.config['MQTT_USERNAME'] = 'pico'
-app.config['MQTT_PASSWORD'] = 'pico'
-app.config['MQTT_KEEPALIVE'] = 5
-app.config['MQTT_TLS_ENABLED'] = False
+socketio = SocketIO (app) 
 
-topic1 = "temperature"
-topic2 = "control"
 
-mqtt = Mqtt(app)
+temp_topic = 'temperature'
+control_topic = 'control'
+server = 'localhost'
+MQTTport = 1883
+client = None
 socketio = SocketIO(app)
 bootstrap = Bootstrap(app)
 
-@mqtt.on_connect()
-def handle_connect(client, userdata, flags, rc):
-   if rc == 0:
-       print('Connected successfully')
-       #print(getIp())
-       print('Subscribed ' + topic1)
-       mqtt.subscribe(topic1)
-       print('Subscribed ' + topic1)
-       mqtt.subscribe(topic2) # subscribe topic
-        # subscribe topic
-   else:
-       print('Bad connection. Code:', rc)
-       
-       
+# SocketIO
+
+@socketio.on('connect')
+def on_connect():
+    print('Socket client connected.')
+    socketio.emit('receive', "Ligao ao broker")
+
+@socketio.on('ledControl')
+def on_publish(payload):
+    client.publish(control_topic, payload)
+    print(payload)
+
+# MQTT
+
+def on_connect(client, userdata, flags, rc):
+
+    socketio.emit('receive', "Status: connected to broker")
+    client.subscribe(temp_topic, qos=0)
+    client.subscribe(control_topic, qos=0)
+
+def on_message(client, userdata, msg):
+    socketio.send(msg.payload.decode())
+    print("On message " + msg.payload.decode())
+
+# HTTP
+
 @app.route('/')
 def index():
     return render_template('index.html')
-
-@app.route('/getIP')
-def getIpAddr():
-    return getIp()
-    
-@socketio.on("ledControl")
-def handle_connect(message):
-    mqtt.publish(topic2, message)
-    print(message)
-    
-@socketio.on('connect')
-def handle_connect():
-    
-    print("Connected to the frontend")
-
-@socketio.on('publish')
-def handle_publish(json_str):
-    data = json.loads(json_str)
-    mqtt.publish(topic1, data['message'])
 
 @app.route('/config')
 def config():
     render_template("config_mqtt.html")
 
-@socketio.on('unsubscribe_all')
-def handle_unsubscribe_all():
-    mqtt.unsubscribe_all()
-
-@mqtt.on_message()
-def handle_mqtt_message(client, userdata, message):
-    print("Received message")
-    print("Topic:" + message.topic + "\n" + "Message:" + message.payload.decode())
-    socketio.emit('new_message', data=message.payload)
-
-@mqtt.on_log()
-def handle_logging(client, userdata, level, buf):
-    print(level, buf)
+# Main
 
 def getIp():
     addresses = netifaces.ifaddresses("enp0s8")
     ip_address = addresses[netifaces.AF_INET][0]['addr']
     return ip_address
-    
-if __name__ == '__main__':
-    socketio.run(app, host= getIp(), port=5001, use_reloader=False, debug=True)
-    
+
+client = mqtt.Client("Mqtt-socket-bridge-2021", clean_session=True)
+client.on_connect = on_connect
+client.on_message = on_message
+client.username_pw_set("pico", "pico")
+client.connect(server, MQTTport, keepalive=60)
+
+print('Starting the MQ loop..')
+client.loop_start()
+
+print('Starting the socket server on port 5000...')
+socketio.run(app, host=getIp(), port=5001, debug=True)
