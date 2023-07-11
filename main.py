c34c633029d2d8ede4a548c1eb915cdfaa4945bc
Rui Paiva <75069807+PaivaX@users.noreply.github.com>
2023-07-11 21:25:47 +0100

    Update main.py
    adicionado codigo novo

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import config
 import functions
 import usocket as socket
 import network
+import uasyncio as asyncio
 
 
 # Entrar no modo de configuração para definir SSID e senha do Wi-Fi
@@ -82,14 +83,14 @@ def setup_mode():
     dns.run_catchall(ip)
 
 # Após sucesso na ligação ao wifi, o RpicoW entra no modo "aplicação"
-def application_mode():
+def application_mode(client):
     # O WebServer agora está ativo
     print("Modo Web Ativo.")
 
     def app_index(request):
         ip_address = connect_to_wifi(wifi_credentials["ssid"], wifi_credentials["password"])
         temperatura = str(pico_temp_sensor.temp)
-        html_content = render_template(f"{config.APP_TEMPLATE_PATH}/index.html", temperature=temperatura, led_status=functions.get_led_status())
+        html_content = render_template(f"{config.APP_TEMPLATE_PATH}/index.html", temperature=temperatura, rega_status=functions.get_led_status())
         return html_content
 
     def app_toggle_led(request):
@@ -97,30 +98,37 @@ def application_mode():
         return app_index(request)
 
     def app_rega_on(request):
-        functions.rega_on()
+        functions.rega_on(client)
         return "ok"
 
     def app_rega_off(request):
-        functions.rega_off()
+        functions.rega_off(client)
         return "ok"
 
     def app_rega_auto(request):
-        functions.rega_auto(request)
+        loop = functions.asyncio.get_event_loop()
+        loop.create_task(functions.rega_auto(request, client))
+        loop.run_forever()
+
         return "ok"
     
     def lcd_update_thread():
         while True:
-            functions.read_temp()
-            utime.sleep(5)  # Aguarda 5 segundos
+            functions.read_temp_lcd() # Envia a temperatura para o LCD
+            try:
+                client.publish("temperature", functions.read_temp()) # Envia a temperatura para o broker
+            except Exception as e:
+                pass
+            utime.sleep(10) # Aguarda 10 segundos
 
-    # Pagina de configuração do MQTT
-    def render_mqtt_config_page(mqtt_server="", mqtt_user="", mqtt_topic=""):
+    # Página de configuração do MQTT
+    def render_mqtt_config_page(mqtt_server="", mqtt_user="", mqtt_password="", mqtt_topic="", temprega=""):
         html_content = """
         <!DOCTYPE html>
         <html>
         <head>
             <meta charset="UTF-8">
-            <title>Configurações MQTT</title>
+            <title>Configurações RPicoW</title>
             <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
             <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
             <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
@@ -148,12 +156,19 @@ def application_mode():
                         <label for="mqttTopic">Tópico Subscrito:</label>
                         <input type="text" class="form-control" id="mqttTopic" name="mqttTopic" placeholder="Tópico subscrito" value="{mqtt_topic}">
                     </div>
-
+                    
+                    <h2 class="mt-4">Outras configurações</h2>
+                    <div class="form-group">
+                        <label for="emprega">Tempo de Rega:</label>
+                        <input type="text" class="form-control" id="temprega" name="temprega" placeholder="Segundos de Rega" value="{temprega}">
+                    </div>
+                    
                     <div class="form-check">
                         <input type="checkbox" class="form-check-input" id="deleteWifiConfig">
                         <label class="form-check-label" for="deleteWifiConfig">Apagar configuração Wi-Fi</label>
                     </div>
 
+                    <button onclick="window.location.href='/'" class="btn btn-primary mt-3">Voltar</button>
                     <button type="submit" class="btn btn-primary mt-3">Guardar</button>
                 </form>
             </div>
@@ -161,7 +176,7 @@ def application_mode():
         </html>
         """
 
-        return html_content.format(mqtt_server=mqtt_server, mqtt_user=mqtt_user, mqtt_topic=mqtt_topic)
+        return html_content.format(mqtt_server=mqtt_server, mqtt_user=mqtt_user, mqtt_password=mqtt_password, mqtt_topic=mqtt_topic, temprega=temprega)
 
     # configuração do MQTT
     def app_mqtt_config(request):
@@ -177,8 +192,10 @@ def application_mode():
                 # Renderizar a página de configurações MQTT com os valores existentes
                 mqtt_server = conf_data.get("mqtt_server", "")
                 mqtt_user = conf_data.get("mqtt_user", "")
+                mqtt_password = conf_data.get("mqtt_password", "")
                 mqtt_topic = conf_data.get("mqtt_topic", "")
-                html_content = render_mqtt_config_page(mqtt_server=mqtt_server, mqtt_user=mqtt_user, mqtt_topic=mqtt_topic)
+                temprega = conf_data.get("temprega", "")
+                html_content = render_mqtt_config_page(mqtt_server=mqtt_server, mqtt_user=mqtt_user, mqtt_password=mqtt_password, mqtt_topic=mqtt_topic, temprega=temprega)
             except OSError:
                 # Renderizar a página de configurações MQTT sem valores
                 html_content = render_mqtt_config_page()
@@ -191,13 +208,16 @@ def application_mode():
             mqtt_user = request.form.get("mqttUser")
             mqtt_password = request.form.get("mqttPassword")
             mqtt_topic = request.form.get("mqttTopic")
+            temprega = request.form.get("temprega")
 
             # Validar e guardar as configurações
             if mqtt_server and mqtt_user and mqtt_topic:
                 conf_data = {
                     "mqtt_server": mqtt_server,
                     "mqtt_user": mqtt_user,
-                    "mqtt_topic": mqtt_topic
+                    "mqtt_password": mqtt_password,
+                    "mqtt_topic": mqtt_topic,
+                    "temprega": temprega
                 }
 
                 # Salvar as informações no arquivo conf.json
@@ -226,6 +246,7 @@ def application_mode():
     
     # Inicia o while do lcd
     _thread.start_new_thread(lcd_update_thread, ())
+    
 
 try:
     os.stat(config.WIFI_FILE)
@@ -258,19 +279,39 @@ try:
         #smtp.quit()
 
                 
-        # Ligar ao Broker MQTT      
+        # Ligar ao Broker MQTT
         try:
             print("A tentar ligar ao Broker MQTT...")
             client = functions.mqtt_connect()
-        #client.connect()
-        #client.subscribe(functions.topic_sub)
-        
-        except OSError as e:            
-                print("Erro ao conectar ao broker.")
+
+        except Exception as e:
+            print("Erro ao conectar e subscrever ao broker MQTT:", e)
+            _thread.start_new_thread(functions.machine_reset, ())
+                #.reconnect()
 
         # Entra no modo da aplicação     
-        application_mode()
+        application_mode(client)
         
+        
+        def start_mqtt_listener(client):
+            loop = functions.asyncio.get_event_loop()
+            loop.create_task(functions.mqtt_listener(client))
+            loop.run_forever()
+            
+        def read_conf_file():
+            with open("conf.json", "r") as file:
+                conf_data = json.load(file)
+            return conf_data
+
+        conf_data = read_conf_file()
+
+        subscribe_mode = conf_data.get("subscribe_mode", "")
+
+        if subscribe_mode == "ON":
+            start_mqtt_listener(client)
+        elif subscribe_mode == "OFF":
+            pass    
+            
         
 except Exception:
     # Se não encontrar as credenciais do Wi-Fi, inicia o modo de configuração
