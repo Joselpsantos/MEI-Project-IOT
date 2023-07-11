c0b17e74f76534c38c566b34770245362103667c
Joselpsantos <joselpsantoss@gmail.com>
2023-07-11 14:38:14 +0100

    Example working

--- WebPage/app.py
+++ WebPage/app.py
@@ -1,71 +1,86 @@
+"""
+
+A small Test application to show how to use Flask-MQTT.
+
+"""
+import logging
+
+import eventlet
+import json
 from flask import Flask, render_template
-import paho.mqtt.client as mqtt
+from flask_mqtt import Mqtt
 from flask_socketio import SocketIO
 from flask_bootstrap import Bootstrap
 import netifaces
-
+eventlet.monkey_patch()
 
 app = Flask(__name__)
-socketio = SocketIO (app) 
+app.config['SECRET'] = 'my secret key'
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config['MQTT_BROKER_URL'] = 'localhost'
+app.config['MQTT_BROKER_PORT'] = 1883
+app.config['MQTT_CLIENT_ID'] = 'flask_mqtt'
+app.config['MQTT_CLEAN_SESSION'] = True
+app.config['MQTT_USERNAME'] = 'pico'
+app.config['MQTT_PASSWORD'] = 'pico'
+app.config['MQTT_KEEPALIVE'] = 5
+app.config['MQTT_TLS_ENABLED'] = False
+app.config['MQTT_LAST_WILL_TOPIC'] = 'home/lastwill'
+app.config['MQTT_LAST_WILL_MESSAGE'] = 'bye'
+app.config['MQTT_LAST_WILL_QOS'] = 2
 
+# Parameters for SSL enabled
+# app.config['MQTT_BROKER_PORT'] = 8883
+# app.config['MQTT_TLS_ENABLED'] = True
+# app.config['MQTT_TLS_INSECURE'] = True
+# app.config['MQTT_TLS_CA_CERTS'] = 'ca.crt'
 
-temp_topic = 'temperature'
-control_topic = 'control'
-server = 'localhost'
-MQTTport = 1883
-client = None
+mqtt = Mqtt(app)
 socketio = SocketIO(app)
 bootstrap = Bootstrap(app)
 
-# SocketIO
-
-@socketio.on('connect')
-def on_connect():
-    print('Socket client connected.')
-    socketio.emit('receive', "Ligao ao broker")
-
-@socketio.on('ledControl')
-def on_publish(payload):
-    client.publish(control_topic, payload)
-    print(payload)
-
-# MQTT
-
-def on_connect(client, userdata, flags, rc):
-
-    socketio.emit('receive', "Status: connected to broker")
-    client.subscribe(temp_topic, qos=0)
-    client.subscribe(control_topic, qos=0)
-
-def on_message(client, userdata, msg):
-    socketio.send(msg.payload.decode())
-    print("On message " + msg.payload.decode())
-
-# HTTP
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/config')
-def config():
-    render_template("config_mqtt.html")
 
-# Main
+@socketio.on('publish')
+def handle_publish(json_str):
+    data = json.loads(json_str)
+    mqtt.publish(data['topic'], data['message'], data['qos'])
+
+
+@socketio.on('subscribe')
+def handle_subscribe(json_str):
+    data = json.loads(json_str)
+    mqtt.subscribe(data['topic'], data['qos'])
+    print("Subscribed: " + data['topic'])
+
+@socketio.on('unsubscribe_all')
+def handle_unsubscribe_all():
+    mqtt.unsubscribe_all()
+
+
+@mqtt.on_message()
+def handle_mqtt_message(client, userdata, message):
+    data = dict(
+        topic=message.topic,
+        payload=message.payload.decode(),
+        qos=message.qos,
+    )
+    socketio.emit('mqtt_message', data=data)
+
+
+@mqtt.on_log()
+def handle_logging(client, userdata, level, buf):
+    print(level, buf)
+    pass
 
 def getIp():
     addresses = netifaces.ifaddresses("enp0s8")
     ip_address = addresses[netifaces.AF_INET][0]['addr']
     return ip_address
 
-client = mqtt.Client("Mqtt-socket-bridge-2021", clean_session=True)
-client.on_connect = on_connect
-client.on_message = on_message
-client.username_pw_set("pico", "pico")
-client.connect(server, MQTTport, keepalive=60)
-
-print('Starting the MQ loop..')
-client.loop_start()
-
-print('Starting the socket server on port 5000...')
-socketio.run(app, host=getIp(), port=5001, debug=True)
+if __name__ == '__main__':
+    socketio.run(app, host=getIp(), port=5000, use_reloader=False, debug=True)
--- WebPage/web_server/app.py
+++ WebPage/web_server/app.py
@@ -1,51 +1,77 @@
-from flask import Flask, render_template, session, copy_current_request_context
-from flask_socketio import SocketIO, emit, disconnect
-from threading import Lock
-import netifaces
+import paho.mqtt.client as mqtt
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, emit
 
-async_mode = None
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socket_ = SocketIO(app, async_mode=async_mode)
-thread = None
-thread_lock = Lock()
+socketio = SocketIO(app)
 
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
 
-@app.route('/')
-def index():
-    return render_template('index.html', async_mode=socket_.async_mode)
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("/esp8266/temperature")
+    client.subscribe("/esp8266/humidity")
 
+# The callback for when a PUBLISH message is received from the ESP8266.
+def on_message(client, userdata, message):
+    #socketio.emit('my variable')
+    print("Received message '" + str(message.payload) + "' on topic '"
+        + message.topic + "' with QoS " + str(message.qos))
+    if message.topic == "/esp8266/temperature":
+        print("temperature update")
+        socketio.emit('dht_temperature', {'data': message.payload})
+    if message.topic == "/esp8266/humidity":
+        print("humidity update")
+        socketio.emit('dht_humidity', {'data': message.payload})
 
-@socket_.on('my_event', namespace='/test')
-def test_message(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': message['data'], 'count': session['receive_count']})
+mqttc=mqtt.Client()
+mqttc.on_connect = on_connect
+mqttc.on_message = on_message
+mqttc.connect("localhost",1883,60)
+mqttc.loop_start()
 
+# Create a dictionary called pins to store the pin number, name, and pin state:
+pins = {
+   4 : {'name' : 'GPIO 4', 'board' : 'esp8266', 'topic' : 'esp8266/4', 'state' : 'False'},
+   5 : {'name' : 'GPIO 5', 'board' : 'esp8266', 'topic' : 'esp8266/5', 'state' : 'False'}
+   }
 
-@socket_.on('my_broadcast_event', namespace='/test')
-def test_broadcast_message(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': message['data'], 'count': session['receive_count']},
-         broadcast=True)
+# Put the pin dictionary into the template data dictionary:
+templateData = {
+   'pins' : pins
+   }
 
+@app.route("/")
+def main():
+   # Pass the template data into the template main.html and return it to the user
+   return render_template('main.html', async_mode=socketio.async_mode, **templateData)
 
-@socket_.on('disconnect_request', namespace='/test')
-def disconnect_request():
-    @copy_current_request_context
-    def can_disconnect():
-        disconnect()
+# The function below is executed when someone requests a URL with the pin number and action in it:
+@app.route("/<board>/<changePin>/<action>")
+def action(board, changePin, action):
+   # Convert the pin from the URL into an integer:
+   changePin = int(changePin)
+   # Get the device name for the pin being changed:
+   devicePin = pins[changePin]['name']
+   # If the action part of the URL is "1" execute the code indented below:
+   if action == "1" and board == 'esp8266':
+      mqttc.publish(pins[changePin]['topic'],"1")
+      pins[changePin]['state'] = 'True'
+   if action == "0" and board == 'esp8266':
+      mqttc.publish(pins[changePin]['topic'],"0")
+      pins[changePin]['state'] = 'False'
+   # Along with the pin dictionary, put the message into the template data dictionary:
+   templateData = {
+      'pins' : pins
+   }
+   return render_template('main.html', **templateData)
 
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': 'Disconnected!', 'count': session['receive_count']},
-         callback=can_disconnect)
+@socketio.on('my event')
+def handle_my_custom_event(json):
+    print('received json data here: ' + str(json))
 
-def getIp():
-    addresses = netifaces.ifaddresses("enp0s8")
-    ip_address = addresses[netifaces.AF_INET][0]['addr']
-    return ip_address
-
-if __name__ == '__main__':
-    socket_.run(app, host=getIp(), port=5001, use_reloader=False, debug=True)
+if __name__ == "__main__":
+   socketio.run(app, host='0.0.0.0', port=8181, debug=True)
